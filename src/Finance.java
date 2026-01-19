7f5be421c91d124eb4e5521c29b43441c9a6652a
esat54 <esatsprx77@gmail.com>
2026-01-19 13:45:30 +0300

    refactor Finance code

--- src/Finance.java
+++ src/Finance.java
@@ -1,50 +1,95 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Finance {
     public static void main(String[] args) {
-        System.out.println("Hoşgeldiniz \nLütfen bir rakam seçiniz");
-
         Scanner scanner = new Scanner(System.in);
-
         double bakiye = 100;
         boolean calisiyor = true;
+        ArrayList<String> gecmis = new ArrayList<>();
+
+        System.out.println("--- Finans Uygulamasına Hoşgeldiniz ---");
 
         while (calisiyor) {
-
-            System.out.println("1- Para Yatır\n2- Para çek\n3- Bakiye sorgulama\n4- Çıkış");
-
-            System.out.print("----------------------------- \n");
+            menuGoster();
             int secim = scanner.nextInt();
-
 
             switch (secim) {
                 case 1 -> {
-                    System.out.print("Yatırmak istediğiniz tutarı giriniz :");
-                    double girenpara = scanner.nextDouble();
-
-                    bakiye += girenpara;
-                    System.out.println("Total bakiyeniz :" + bakiye);
+                    System.out.print("Yatırmak istediğiniz tutar: ");
+                    double miktar = scanner.nextDouble();
+                    bakiye = paraYatir(bakiye, miktar);
+                    bakiyeGoster(bakiye);
+                    gecmis.add(miktar + " TL para yatırıldı");
                 }
-
                 case 2 -> {
-                    System.out.print("Çekmek istediğiniz tutarı giriniz :");
-                    double cekilenpara = scanner.nextDouble();
-
-                    if (bakiye < cekilenpara) {
-                        System.out.println("Bakiye yeresiz");
+                    System.out.print("Çekmek istediğiniz tutar: ");
+                    double miktar = scanner.nextDouble();
+                    bakiye = paraCek(bakiye, miktar);
+                    bakiyeGoster(bakiye);
+                    gecmis.add(miktar + " TL para çekildi");
+                }
+                case 3 -> bakiyeGoster(bakiye);
+                case 4 -> {
+                    System.out.println("--- SON 5 İŞLEM ---");
+                    if (gecmis.isEmpty()) {
+                        System.out.println("Henüz bir işlem yapılmamış");
                     } else {
-                        bakiye -= cekilenpara;
-                        System.out.println("Total bakiyeniz :" + bakiye);
+                        int toplamislem = gecmis.size();
+                        int baslangic = Math.max(0, toplamislem - 5);
+
+
+                        for (int i = baslangic; i < toplamislem; i++) {
+                            System.out.println("- " + gecmis.get(i));
+                        }
+                        ;
                     }
                 }
-
-                case 3 -> System.out.println("Total Bakiyeniz :" + bakiye);
-
-                case 4 -> {
-                    System.out.println("Çıkış yapılıyor..");
+                case 5 -> {
+                    System.out.println("Çıkış yapılıyor. İyi günler!");
                     calisiyor = false;
                 }
+                default -> System.out.println("Hatalı seçim! Lütfen 1-5 arası bir sayı giriniz.");
             }
+        }
+        scanner.close();
+    }
+
+
+    public static void menuGoster() {
+        System.out.println("\n1- Para Yatır");
+        System.out.println("2- Para Çek");
+        System.out.println("3- Bakiye Sorgulama");
+        System.out.println("4- Geçmiş 5 işlemi göster");
+        System.out.println("5- Çıkış");
+        System.out.print("Seçiminiz: ");
+    }
+
+    public static void bakiyeGoster(double miktar) {
+        System.out.println("Güncel Bakiyen: " + miktar + " TL");
+    }
+
+    public static double paraYatir(double bakiye, double miktar) {
+        if (miktar > 0) {
+            return miktar + bakiye;
+        } else {
+            System.out.println("Yatırılacak miktar 0'dan büyük olmalıdır.");
+            return bakiye;
+        }
+    }
+
+    public static double paraCek(double mevcutBakiye, double miktar) {
+        if (miktar <= 0) {
+            System.out.println("Hata: Geçersiz miktar.");
+            return mevcutBakiye;
+        }
+
+        if (miktar > mevcutBakiye) {
+            System.out.println("Hata: Yetersiz bakiye! İşlem gerçekleştirilemedi.");
+            return mevcutBakiye;
+        } else {
+            System.out.println("İşlem başarılı. " + miktar + " TL çekildi.");
+            return mevcutBakiye - miktar;
         }
     }
 }
